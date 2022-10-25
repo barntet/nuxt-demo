2de3a37e32e3b00ba04ff6990b32aa9e8201d5e1
罗旭宏 <barntet@163.com>
2022-10-25 18:02:23 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <Content />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Header from "@/components/header.vue";
+import contentA from "@/components/content.vue";
+
+export default Vue.extend({
+  components: {
+    Header,
+    contentA,
+  },
+  name: "IndexPage",
+});
+</script>
